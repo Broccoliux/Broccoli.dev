5684617773226a13aec934304e8c85d66dbed5ee
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-05 04:05:19 +0500

    idk eiter

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+// Typed.js Animation
+var typed = new Typed('#element', {
+    strings: ['AI/ML Engineer', 'IOT Engineer.', 'Embedded systems developer.'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
