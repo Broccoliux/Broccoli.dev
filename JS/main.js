06505c3ff3f5dd652ad30516bb16afb6a939bcee
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-07 14:16:32 +0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -17,7 +17,7 @@ const dock = document.getElementById("dock");
 const items = [...document.querySelectorAll(".dock-item")];
 
 const ICON_SIZE = 45;
-const MAX_SCALE = 3.54;
+const MAX_SCALE = 2.1;
 const MAGNETIC_DISTANCE = 150;
 
 let mouseX = Infinity;
