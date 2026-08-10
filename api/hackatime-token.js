8fed9b5ed867d0a24d858d0819aa02e4d95f8c71
Nabeel Ahmed <fussdeek@gmail.com>
2026-08-10 13:39:11 +0500

    Update hackatime-token.js

--- api/hackatime-token.js
+++ api/hackatime-token.js
@@ -19,7 +19,27 @@ export default async function handler(req, res) {
       });
     }
 
-    const 
+    const response = await fetch(
+      "https://hackatime.hackclub.com/oauth/token",
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+
+        body: new URLSearchParams({
+          client_id,
+          code,
+          code_verifier,
+          redirect_uri,
+          grant_type: "authorization_code"
+        })
+      }
+    );
+
+    const data = await response.json();
+    return res.status(response.status).json(data);
 
 
 
