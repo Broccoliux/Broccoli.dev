74c75e88e1cd0189937ee1db7f5c28f5063a0a83
Nabeel Ahmed <fussdeek@gmail.com>
2026-08-21 12:42:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import os
 import json
 import smtplib
 from email.message impott EmailMessage
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from google import genai #using google genai sdk
+
+app = FastApI(title="Broccoli 2")
+
+
